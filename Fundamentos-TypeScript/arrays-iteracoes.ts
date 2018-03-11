e069953e991e8eb14f87ea4b0de9791f6847b386
Valdir Júnior Cordeiro Rocha <vjcordeiror@gmail.com>
2018-03-11 20:11:37 -0300

    iteração de arrays

---
+++ Fundamentos-TypeScript/arrays-iteracoes.ts
@@ -0,0 +1,11 @@
+let notasAlunos: number[] = [10, 8, 8.5, 7];
+
+//Indices
+for(let i in notasAlunos){
+    console.log(i);
+}
+
+//Valores -- of
+for(let notaAluno of notasAlunos){
+    console.log(notaAluno);
+}
